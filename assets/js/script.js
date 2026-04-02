978559ca8d30c3ff1cf45363ea24f3446ef67ddb
Marco-Petronella <marco.petronella85@gmail.com>
2026-04-02 12:30:26 +0200

    create list ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,8 @@ Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 */
 
-
 //Creo una funzione che usando l'api di boolean prende una e-mail
 const emailApi = "https://flynn.boolean.careers/exercises/api/random/mail";
-function getEmail() {
-  fetch(emailApi)
-    .then((response) => response.json())
-    .then((emailData) => {
-      console.log(emailData);
-      return(emailData.response)
-    });
-}
 
 //prendo il contenitore unordered list all'interno della pagina
 const listContainer = document.querySelector("ul");
@@ -27,17 +18,21 @@ const listContainer = document.querySelector("ul");
 //uso una funzione in virtù del bonus così potro richiamarlo più volte con il bottone ed eventualmente anche variare il numero di e-mail
 //la creazione dell'elemento LI lo metterò in un'altra funzione a parte ancora per dividere i compiti nelle funzioni
 
-function addEmail(num) {
+function addEmail(num) {}
+function addLiElement(email) {
+  const liElement = document.createElement("li");
+  liElement.innerHTML = email;
+  listContainer.appendChild(liElement);
+}
+getEmail(2);
+
+function getEmail(num) {
   for (let i = 0; i < num; i++) {
-    const email = getEmail();
-    console.log(email);
-    addLiElement(email);
+    fetch(emailApi)
+      .then((response) => response.json())
+      .then((emailData) => {
+        const email = emailData.response;
+        addLiElement(email);
+      });
   }
 }
-function addLiElement(email) {
-    const liElement = document.createElement("li");
-    liElement.innerHTML = email;
-    listContainer.appendChild(liElement)
-}
-addEmail(2)
- 
